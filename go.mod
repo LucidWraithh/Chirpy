0cc785ffb5687e01f3d0caf443ed2233dfa83d43
LucidWraithh <razor0916@gmail.com>
2024-04-30 19:38:06 -0500

    got server up and running, currently it always returns an error 404

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LucidWraithh/Chirpy
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+
+func main(){
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Handler: mux,
+		Addr: "localhost:8080",
+	}
+
+
+	server.ListenAndServe()
+}
